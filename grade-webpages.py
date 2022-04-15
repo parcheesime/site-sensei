b4d1c7a7826e351ea3eaf121e97778efff8ef47f
Aletia Trepte <aletia@zoho.com>
2022-04-14 20:58:36 -0700

    Added function write html file with feedback.

--- grade-webpages.py
+++ grade-webpages.py
@@ -3,14 +3,11 @@
 # extract tags and frequency used, extract links and broken links
 
 
-import ssl\
-
-import requests
+import ssl
 import socket
 import urllib.error
 import urllib.parse
 import urllib.request
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,6 +17,31 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 
+# Retrieve tags frequency, using urlopen()
+def get_tags(url):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'li', 'img']
+    results = {}
+    for tag in tags:
+        t = soup(tag)
+        if tag not in soup(tag):
+            results[tag] = 0
+        results[tag] += len(t)
+    return results
+
+
+# Check for CSS clss selectors, using requests.get
+def get_class(url):
+    page = requests.get(url)
+    data = page.text
+    data = data.split()
+    for ele in data:
+        if 'class' in ele:
+            print(ele)
+
+
+# check for broken links
 def link_status(url):
     page = requests.get(url)
     response_code = str(page.status_code)
@@ -44,48 +66,23 @@ def get_links(url):
     return urllist
 
 
-# Retrieve tags frequency
-def get_tags(url):
-    html = urllib.request.urlopen(url, context=ctx).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'li', 'img']
-    results = {}
-    for tag in tags:
-        t = soup(tag)
-        if tag not in soup(tag):
-            results[tag] = 0
-        results[tag] += len(t)
-    return results
-
-
-def get_class(url):
-    page = requests.get(url)
-    data = page.text
-    data = data.split()
-    for ele in data:
-        if 'class' in ele:
-            print(ele)
-
-
-def get_soup(url):
-    html = urllib.request.urlopen(url, context=ctx).read()
-    soup1 = BeautifulSoup(html, 'html.parser')
-    print(soup1)
-    page = requests.get(url)
-    data = page.text
-    soup2 = BeautifulSoup(data, 'lxml')
-    print(soup2)
-
-
 def check_localhost():
     localhost = socket.gethostbyname('localhost')
     return localhost == '127.0.0.1'
 
 
-def check_connectivity():
-    request = requests.get("http://www.google.com")
-    response_code = (request.status_code)
+# Check url connectivity
+def check_connectivity(url):
+    request = requests.get(url)
+    response_code = request.status_code
     return response_code == 200
 
-print(check_localhost())
-print(check_connectivity())
+
+# Create HTML page with results
+def html_output(feedback):
+    with open('MST.html', 'w') as html_pg:
+        html_pg.write('<html>\n<head>\n<title> \n Teacher Feedback for Web Project \
+           </title>\n</head> <body><h1>Your Results for the<u>Web Page Project</u></h1>\
+           \n<h2>A <u>CS</u> {} </h2> \n</body></html>'.format(feedback))
+
+html_output("good job")
